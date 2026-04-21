17e0d8ec0a6aa3e5387d7a1a42b2102161cb2fcf
Sandeep Miriyala <41898282+sandeepmiriyala03@users.noreply.github.com>
2026-04-21 14:44:43 +0530

    code fixed

---
+++ tsup.config.ts
@@ -0,0 +1,27 @@
+﻿import { defineConfig } from "tsup";
+
+export default defineConfig([
+  {
+    entry: ["src/index.ts"],
+    format: ["esm", "cjs"],
+    dts: true,
+    clean: true,
+    minify: true,
+    external: ["react", "react-dom"],
+  },
+  {
+    entry: { "index.global": "src/index.ts" },
+    format: ["iife"],
+    globalName: "YuktAI",
+    outDir: "dist",
+    minify: true,
+    platform: "browser",
+    external: ["react", "react-dom"],
+    esbuildOptions(options) {
+      options.define = {
+        "process.env.NODE_ENV": '"production"',
+        "process": '{"env":{"NODE_ENV":"production"}}',
+      };
+    },
+  },
+]);
