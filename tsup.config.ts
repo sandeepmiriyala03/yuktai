e8566c4aa84fabd827ef898a70724503c2c3071a
Sandeep Miriyala <41898282+sandeepmiriyala03@users.noreply.github.com>
2026-04-21 15:17:50 +0530

    ok code

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,25 +2,26 @@
 
 export default defineConfig([
   {
-    entry: ["src/index.ts"],
-    format: ["esm", "cjs"],
-    dts: true,
-    clean: true,
-    minify: true,
+    entry:    ["src/index.ts"],
+    format:   ["esm", "cjs"],
+    dts:      true,
+    clean:    true,
+    minify:   true,
     external: ["react", "react-dom"],
   },
   {
-    entry: { "index.global": "src/index.ts" },
-    format: ["iife"],
+    entry:      { "index.global": "src/index.ts" },
+    format:     ["iife"],
     globalName: "YuktAI",
-    outDir: "dist",
-    minify: true,
-    platform: "browser",
-    external: ["react", "react-dom"],
+    outDir:     "dist",
+    minify:     true,
+    dts:        false,
+    platform:   "browser",
+    external:   ["react", "react-dom", "tesseract.js", "idb-keyval", "wasm-feature-detect"],
     esbuildOptions(options) {
       options.define = {
         "process.env.NODE_ENV": '"production"',
-        "process": '{"env":{"NODE_ENV":"production"}}',
+        "process":              '{"env":{"NODE_ENV":"production"}}',
       };
     },
   },
