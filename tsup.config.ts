7210900efa70cb51c9ae2c2ad4d22f815d952eb1
Sandeep Miriyala <41898282+sandeepmiriyala03@users.noreply.github.com>
2026-04-25 11:25:55 +0530

    ok cod e

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig([
   {
     entry: ["src/index.ts"],
     format: ["esm", "cjs"],
-    dts: false,
+    dts: true,
     clean: true,
     minify: true,
     external: ["react", "react-dom"],
